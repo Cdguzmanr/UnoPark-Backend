f4db6c6e76179c8fb747fb246b683a5f92b42386
Carlos Guzmán Ramírez <carlosd.gr.ed@gmail.com>
2024-02-21 22:11:16 -0600

    Built Migration Handler

---
+++ TEAM11.UNO.MigrationHandler/Program.cs
@@ -0,0 +1,18 @@
+﻿using TEAM11.UNO.PL.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddDbContextPool<UNOEntities>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("UNOConnection"), builder => {
+    builder.EnableRetryOnFailure(5, TimeSpan.FromSeconds(10), null);
+}));
+
+var app = builder.Build();
+app.Run();
+
+
---
+++ TEAM11.UNO.PL.Test/GlobalUsings.cs
@@ -0,0 +1,16 @@
+global using Microsoft.VisualStudio.TestTools.UnitTesting;
+global using Microsoft.EntityFrameworkCore.Storage;
+global using Microsoft.Data.SqlClient;
+global using Microsoft.EntityFrameworkCore;
+global using Newtonsoft.Json;
+global using System.Diagnostics;
+global using System.Reflection;
+global using System;
+global using System.Collections.Generic;
+global using System.Linq;
+global using System.Text;
+global using System.Threading.Tasks;
+global using Microsoft.Extensions.Configuration;
+global using TEAM11.UNO.PL.Data;
+global using TEAM11.UNO.PL.Entities;
+global using TEAM11.UNO.PL;
