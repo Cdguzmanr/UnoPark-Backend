47e1d703a3c4c628214f6fbdf44c5b1cebaae136
Carlos Guzmán Ramírez <carlosd.gr.ed@gmail.com>
2024-02-21 23:26:39 -0600

    PL Test completed. There is an error in utBase with UseLazyLoadingProxies(). Thus, the tests don't run properly, but all tests have appropiate code.

--- TEAM11.UNO.PL.Test/utPlayer.cs
+++ TEAM11.UNO.PL.Test/utPlayer.cs
@@ -6,7 +6,53 @@ using System.Threading.Tasks;
 
 namespace TEAM11.UNO.PL.Test
 {
-    internal class utPlayer
+    [TestClass]
+    public class utPlayer : utBase<tblPlayer>
     {
+        [TestMethod]
+        public void LoadTest()
+        {
+            int expected = 0;
+            var player = base.LoadTest();
+            Assert.AreEqual(expected, player.Count());
+        }
+
+        [TestMethod]
+        public void InsertTest()
+        {
+            tblPlayer row = new tblPlayer();
+            row.Id = Guid.NewGuid();
+            row.IsComputerPlayer = false;
+            row.UserId = base.LoadTest().FirstOrDefault().UserId; // Foreign Key
+            row.GameId = base.LoadTest().FirstOrDefault().GameId; // Foreign Key
+
+            int results = base.InsertTest(row);
+            Assert.IsTrue(results > 0);
+        }
+
+        [TestMethod]
+        public void UpdateTest()
+        {
+            tblPlayer row = base.LoadTest().FirstOrDefault();
+
+            if (row != null)
+            {
+                row.IsComputerPlayer = true;
+                int results = base.UpdateTest(row);
+                Assert.IsTrue(results > 0);
+            }
+        }
+
+        [TestMethod]
+        public void DeleteTest()
+        {
+            tblPlayer row = base.LoadTest().FirstOrDefault();
+
+            if (row != null)
+            {
+                int results = base.DeleteTest(row);
+                Assert.IsTrue(results > 0);
+            }
+        }
     }
 }
--- TEAM11.UNO.PL.Test/utUser.cs
+++ TEAM11.UNO.PL.Test/utUser.cs
@@ -1,12 +1,60 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace TEAM11.UNO.PL.Test
+﻿namespace TEAM11.UNO.PL.Test
 {
-    internal class utUser
+    [TestClass]
+    public class utUser : utBase<tblUser>
     {
+
+        [TestMethod]
+        public void LoadTest()
+        {
+            Assert.IsTrue(base.LoadTest().Count() > 0);
+        }
+
+        [TestMethod]
+        public void InsertTest()
+        {
+            tblUser newRow = new tblUser();
+
+            newRow.Id = Guid.NewGuid();
+            newRow.FirstName = "Joe";
+            newRow.LastName = "Billings";
+            newRow.Username = "XXXXXX";
+            newRow.Password = "YYYYY";
+
+            int rowsAffected = InsertTest(newRow);
+
+            Assert.AreEqual(1, rowsAffected);
+        }
+
+        [TestMethod]
+        public void UpdateTest()
+        {
+            tblUser row = base.LoadTest().FirstOrDefault();
+
+            if (row != null)
+            {
+                row.FirstName = "Sarah";
+                row.LastName = "Vicchiollo";
+                int rowsAffected = UpdateTest(row);
+
+                Assert.AreEqual(1, rowsAffected);
+            }
+        }
+
+
+        [TestMethod]
+        public void DeleteTest()
+        {
+            tblUser row = base.LoadTest().FirstOrDefault();
+
+            if (row != null)
+            {
+                dc.tblUsers.Remove(row);
+                int rowsAffected = DeleteTest(row);
+
+                Assert.IsTrue(rowsAffected == 1);
+            }
+
+        }
     }
 }
