f5062c308e5966a257016d4d1cd1496dc1875100
Austin Steffes <austinjsteffes@gmail.com>
2024-03-06 11:22:42 -0600

    Creating out the bl test layer, also added nuget packages to solution and not project.

---
+++ TEAM11.UNO.BL.Test/utCard.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TEAM11.UNO.BL.Test
+{
+    [TestClass]
+    public class utCard : utBase
+    {
+        [TestMethod]
+        public void LoadTest()
+        {
+
+        }
+        [TestMethod]
+        public void LoadByIdTest()
+        {
+
+        }
+        [TestMethod]
+        public void InsertTest()
+        {
+
+        }
+        [TestMethod]
+        public void UpdateTest()
+        {
+
+        }
+        [TestMethod]
+        public void DeleteTest()
+        {
+
+        }
+    }
+}
---
+++ TEAM11.UNO.BL.Test/utUser.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TEAM11.UNO.BL.Test
+{
+    public class utUser : utBase
+    {
+        [TestMethod]
+        public void LoadTest()
+        {
+
+        }
+        [TestMethod]
+        public void LoadByIdTest()
+        {
+
+        }
+        [TestMethod]
+        public void InsertTest()
+        {
+
+        }
+        [TestMethod]
+        public void UpdateTest()
+        {
+
+        }
+        [TestMethod]
+        public void DeleteTest()
+        {
+
+        }
+    }
+}
