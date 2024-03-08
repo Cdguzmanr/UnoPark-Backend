53bc5d418f3ff686bf21114f695ff3b485b28720
Carlos Guzmán Ramírez <carlosd.gr.ed@gmail.com>
2024-03-07 21:51:46 -0600

    Created BL Manager files

---
+++ TEAM11.UNO.BL/PlayerManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TEAM11.UNO.BL
+{
+    public class PlayerManager : GenericManager<tblPlayer>
+    {
+
+        public PlayerManager(DbContextOptions<UNOEntities> options) : base(options)
+        {
+        }
+    }
+}
