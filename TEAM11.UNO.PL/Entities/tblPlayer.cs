bbbe80c600edfb316e4ea1b6ac96d79f6f45931d
Austin Steffes <austinjsteffes@gmail.com>
2024-03-18 13:28:24 -0500

    changing around properties

--- TEAM11.UNO.PL/Entities/tblPlayer.cs
+++ TEAM11.UNO.PL/Entities/tblPlayer.cs
@@ -12,14 +12,15 @@ namespace TEAM11.UNO.PL.Entities
         public bool IsComputerPlayer { get; set; }
 
         // Connection among tables
+
+        // Junction table connectors
         public Guid UserId { get; set; }
-        public Guid GameId { get; set; }
         public virtual tblUser User { get; set; }
+
+        public Guid GameId { get; set; }
         public virtual tblGame Game { get; set; }
 
         public virtual ICollection<tblPlayerCard> Playercards { get; set; }
-
-        // Foreign key relationship to tblGame.
         public virtual ICollection<tblGame> Games { get; set; }
     }
 }
