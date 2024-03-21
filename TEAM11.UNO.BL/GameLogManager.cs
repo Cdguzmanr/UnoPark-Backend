b63ec8af5f863c6c8b7d193e0534bfa4d8c6ae1c
Carlos Guzmán Ramírez <carlosd.gr.ed@gmail.com>
2024-03-21 18:24:34 -0500

    Completed GameLogManager

--- TEAM11.UNO.BL/GameLogManager.cs
+++ TEAM11.UNO.BL/GameLogManager.cs
@@ -12,5 +12,103 @@ namespace TEAM11.UNO.BL
         public GameLogManager(DbContextOptions<UNOEntities> options) : base(options)
         {
         }
+
+
+        public int Insert(GameLog card, bool rollback = false)
+        {
+            try
+            {
+                tblGameLog row = new tblGameLog { Id = card.Id, GameId = card.GameId, Description = card.Description, Timestamp = card.Timestamp}; // GameId, Description, Timestamp
+                card.Id = row.Id;
+                return base.Insert(row, rollback);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        public int Update(GameLog card, bool rollback = false)
+        {
+            try
+            {
+                return base.Update(new tblGameLog
+                {
+                    Id = card.Id,
+                    GameId = card.GameId,
+                    Description = card.Description,
+                    Timestamp = card.Timestamp
+                },
+                rollback);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        public int Delete(Guid id, bool rollback = false)
+        {
+            try
+            {
+                return base.Delete(id, rollback);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        public List<GameLog> Load()
+        {
+            try
+            {
+                List<GameLog> rows = new List<GameLog>();
+                base.Load()
+                    .ForEach(d => rows.Add(
+                        new GameLog
+                        {
+                            Id = d.Id,
+                            GameId = d.GameId,
+                            Description = d.Description,
+                            Timestamp = d.Timestamp
+                        }));
+
+                return rows;
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        public GameLog LoadById(Guid id)
+        {
+            try
+            {
+
+                tblGameLog row = base.LoadById(id);
+
+                if (row != null)
+                {
+                    GameLog card = new GameLog
+                    {
+                        Id = row.Id,
+                        GameId = row.GameId,
+                        Description = row.Description,
+                        Timestamp = row.Timestamp
+                    };
+                    return card;
+                }
+                else
+                {
+                    throw new Exception("Row was not found.");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
     }
 }
+
