8f8caa7fc8dd0660c809678fd6dda03f7a7b1094
Austin Steffes <austinjsteffes@gmail.com>
2024-04-09 09:04:37 -0500

    Added in Controllers.

---
+++ TEAM11.UNO.API/Controllers/GameController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TEAM11.UNO.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GameController : ControllerBase
+    {
+
+    }
+}
