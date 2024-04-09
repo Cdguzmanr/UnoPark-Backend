041d374850121bc3f89605b88d1553a8cfcf307a
Austin Steffes <austinjsteffes@gmail.com>
2024-04-09 17:50:19 -0500

    Continued work on the test layers getting them to all function and work properly.

--- TEAM11.UNO.BL.Test/utCard.cs
+++ TEAM11.UNO.BL.Test/utCard.cs
@@ -9,48 +9,48 @@ namespace TEAM11.UNO.BL.Test
     [TestClass]
     public class utCard : utBase
     {
-        //[TestMethod]
-        //public void LoadTest()
-        //{
-        //    List<Card> cards = new CardManager(options).Load();
+        [TestMethod]
+        public void LoadTest()
+        {
+            List<Card> cards = new CardManager(options).Load();
 
-        //    int expected = 1;
+            int expected = 54;
 
-        //    Assert.AreEqual(expected, cards.Count);
-        //}
-        //[TestMethod]
-        //public void LoadByIdTest()
-        //{
-        //    Card card = new CardManager(options).Load().FirstOrDefault();
-        //    Assert.AreEqual(new CardManager(options).LoadById(card.Id).Id, card.Id);
-        //}
-        //[TestMethod]
-        //public void InsertTest()
-        //{
-        //    Card card = new Card
-        //    {
-        //        card.Name = "Test",
-        //        card.Color = "Magenta",
-        //        card.Type = "Kewl"
-        //    }
+            Assert.AreEqual(expected, cards.Count);
+        }
+        [TestMethod]
+        public void LoadByIdTest()
+        {
+            Card card = new CardManager(options).Load().FirstOrDefault();
+            Assert.AreEqual(new CardManager(options).LoadById(card.Id).Id, card.Id);
+        }
+        [TestMethod]
+        public void InsertTest()
+        {
+            Card card = new Card
+            {
+                Name = "Test",
+                Color = "Magenta",
+                Type = "Kewl"
+            };
 
-        //    int result = new CardManager(options).Insert(card, true);
-        //    Assert.IsTrue(result > 0);
-        //}
-        //[TestMethod]
-        //public void UpdateTest()
-        //{
-        //    Card card = new CardManager(options).Load().FirstOrDefault();
-        //    card.Name = "BipityBoBopBop";
+            int result = new CardManager(options).Insert(card, true);
+            Assert.IsTrue(result > 0);
+        }
+        [TestMethod]
+        public void UpdateTest()
+        {
+            Card card = new CardManager(options).Load().FirstOrDefault();
+            card.Name = "BipityBoBopBop";
 
-        //    Assert.IsTrue(new CardManager(options).Update(card, true) > 0);
-        //}
-        //[TestMethod]
-        //public void DeleteTest()
-        //{
-        //    Card card = new CardManager(options).Load().LastOrDefault();
+            Assert.IsTrue(new CardManager(options).Update(card, true) > 0);
+        }
+        [TestMethod]
+        public void DeleteTest()
+        {
+            Card card = new CardManager(options).Load().LastOrDefault();
 
-        //    Assert.IsTrue(new CardManager(options).Delete(card.Id, true) > 0);
-        //}
+            Assert.IsTrue(new CardManager(options).Delete(card.Id, true) > 0);
+        }
     }
 }
--- TEAM11.UNO.BL.Test/utUser.cs
+++ TEAM11.UNO.BL.Test/utUser.cs
@@ -7,51 +7,52 @@ using System.Threading.Tasks;
 
 namespace TEAM11.UNO.BL.Test
 {
+    [TestClass]
     public class utUser : utBase
     {
-        //[TestMethod]
-        //public void LoadTest()
-        //{
-        //    List<User> users = new UserManager(options).Load();
+        [TestMethod]
+        public void LoadTest()
+        {
+            List<User> users = new UserManager(options).Load();
 
-        //    int expected = 1;
+            int expected = 4;
 
-        //    Assert.AreEqual(expected, users.Count);
-        //}
-        //[TestMethod]
-        //public void LoadByIdTest()
-        //{
-        //    User user = new UserManager(options).Load().FirstOrDefault();
-        //    Assert.AreEqual(new UserManager(options).LoadById(user.Id).Id, user.Id);
-        //}
-        //[TestMethod]
-        //public void InsertTest()
-        //{
-        //    User user = new User
-        //    {
-        //        user.Username = "Test",
-        //        user.Password = "Test",
-        //        user.FirstName = "Test",
-        //        user.LastName = "Test",
-        //    }
+            Assert.AreEqual(expected, users.Count);
+        }
+        [TestMethod]
+        public void LoadByIdTest()
+        {
+            User user = new UserManager(options).Load().FirstOrDefault();
+            Assert.AreEqual(new UserManager(options).LoadById(user.Id).Id, user.Id);
+        }
+        [TestMethod]
+        public void InsertTest()
+        {
+            User user = new User
+            {
+                Username = "Test",
+                Password = "Test",
+                FirstName = "Test",
+                LastName = "Test",
+            };
 
-        //    int result = new UserManager(options).Insert(user, true);
-        //    Assert.IsTrue(result > 0);
-        //}
-        //[TestMethod]
-        //public void UpdateTest()
-        //{
-        //    User user = new UserManager(options).Load().FirstOrDefault();
-        //    user.FirstName = "Nemo";
+            int result = new UserManager(options).Insert(user, true);
+            Assert.IsTrue(result > 0);
+        }
+        [TestMethod]
+        public void UpdateTest()
+        {
+            User user = new UserManager(options).Load().FirstOrDefault();
+            user.FirstName = "Nemo";
 
-        //    Assert.IsTrue(new UserManager(options).Update(user, true) > 0);
-        //}
-        //[TestMethod]
-        //public void DeleteTest()
-        //{
-        //    User user = new UserManager(options).Load().LastOrDefault();
+            Assert.IsTrue(new UserManager(options).Update(user, true) > 0);
+        }
+        [TestMethod]
+        public void DeleteTest()
+        {
+            User user = new UserManager(options).Load().LastOrDefault();
 
-        //    Assert.IsTrue(new UserManager(options).Delete(user.Id, true) > 0);
-        //}
+            Assert.IsTrue(new UserManager(options).Delete(user.Id, true) > 0);
+        }
     }
 }
