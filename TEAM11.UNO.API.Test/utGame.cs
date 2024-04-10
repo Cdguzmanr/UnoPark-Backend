0e00d747546136e84810f5402edfc67efe64c13b
Austin Steffes <austinjsteffes@gmail.com>
2024-04-09 19:12:37 -0500

    Okay, I just need to adequately modify the methods to involve all the fields required to Update. Otherwise lifes good here now.

--- TEAM11.UNO.API.Test/utGame.cs
+++ TEAM11.UNO.API.Test/utGame.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TEAM11.UNO.BL;
 using TEAM11.UNO.BL.Models;
 
 namespace TEAM11.UNO.API.Test
