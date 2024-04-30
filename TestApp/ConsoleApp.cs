3a0a5e2635bd2121ef6a152418934deb2c44598e
Austin Steffes <austinjsteffes@gmail.com>
2024-04-30 15:18:59 -0500

    Working on creating Signal R with Console Application now.

---
+++ TestApp/ConsoleApp.cs
@@ -0,0 +1,90 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using TEAM11.UNO.ConsoleApp;
+using Newtonsoft.Json;
+
+namespace UnoCardRetriever
+{
+    class ConsoleApp
+    {
+        static async Task Main(string[] args)
+        {
+            try
+            {
+                // Creating local variables needed to figure out if Signal R is working or not.
+                string user = "Uno";
+                string localAddress = "https://localhost:7045/UnoHub";
+
+                // Calling Signal R address.
+                var signalRConnection = new SignalRConnection(localAddress);
+
+                Console.WriteLine("Before Signal R is Called");
+                signalRConnection.ConnectToChannel(user);
+                Console.WriteLine("After Signal R is Called");
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred: {ex.Message}");
+            }
+
+
+            //try
+            //{
+            //    // Base URL of the API
+            //    string baseUrl = "https://bigprojectapi-300079087.azurewebsites.net";
+
+            //    // API endpoint for retrieving cards
+            //    string endpoint = "/api/Card";
+
+            //    // Create HttpClient instance
+            //    using (var client = new HttpClient())
+            //    {
+            //        client.BaseAddress = new Uri(baseUrl);
+            //        client.DefaultRequestHeaders.Accept.Clear();
+            //        client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            //        // Call the API to retrieve the list of cards
+            //        HttpResponseMessage response = await client.GetAsync(endpoint);
+
+            //        if (response.IsSuccessStatusCode)
+            //        {
+            //            // Parse the response content
+            //            var cardJson = await response.Content.ReadAsStringAsync();
+            //            var cards = JsonConvert.DeserializeObject<List<Card>>(cardJson);
+
+            //            // Display the retrieved cards
+            //            Console.WriteLine("Retrieved Cards:");
+            //            foreach (var card in cards)
+            //            {
+            //                Console.WriteLine($"ID: {card.Id}, Name: {card.Name}, Color: {card.Color}, Type: {card.Type}");
+            //            }
+            //        }
+            //        else
+            //        {
+            //            Console.WriteLine($"Failed to retrieve cards. Status code: {response.StatusCode}");
+            //        }
+            //    }
+            //}
+            //catch (Exception ex)
+            //{
+            //    Console.WriteLine($"An error occurred: {ex.Message}");
+            //}
+
+            //Console.ReadLine();
+        }
+    }
+
+    // Define Card class to match the structure of the JSON response
+    //public class Card
+    //{
+    //    public Guid Id { get; set; }
+    //    public string Name { get; set; }
+    //    public string Color { get; set; }
+    //    public string Type { get; set; }
+    //}
+}
---
+++ TestApp/SignalRConnection.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+
+namespace TEAM11.UNO.ConsoleApp
+{
+    public class SignalRConnection
+    {
+        private string hubAddress;
+        HubConnection _connection;
+        string user;
+
+        public SignalRConnection(string hubAddress)
+        {
+            this.hubAddress = hubAddress;
+        }
+
+        public void Start()
+        {
+            _connection = new HubConnectionBuilder()
+                .WithUrl(hubAddress)
+                .Build();
+
+            _connection.On<string, string>("ReceiveMessage", (s1, s2) => OnSend(s1, s2));
+
+            _connection.StartAsync();
+        }
+
+        private void OnSend(string user, string message)
+        {
+            Console.WriteLine(user + ": " + message);
+        }
+
+        public void SendMessageToChannel(string user, string message)
+        {
+
+            try
+            {
+                Console.WriteLine("Sending message " + message + " from " + user);
+                _connection.InvokeAsync("SendMessage", user, message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+
+
+        }
+
+        public void ConnectToChannel(string user)
+        {
+            Start();
+            string message = user + " Connected";
+
+            try
+            {
+                _connection.InvokeAsync("SendMessage", "System", message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+    }
+}
